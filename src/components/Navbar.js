f415561de0251f1e6def9373b296a15f9adf7ad8
Puram Deepak <73322656+PuramDeepak@users.noreply.github.com>
2023-05-29 11:53:08 +0530

    commited

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,14 +10,14 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">{props.title}</a>
+        <a className="navbar-brand" href="#">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item mx-1">
-              <a  className="nav-link active" aria-current="page" href="/">Home</a>
+              <a  className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             {/* <Link className="nav-link active" to="/about">About</Link> */}
           </ul>
