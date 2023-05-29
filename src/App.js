d15fc9b0d36113d81b639884be357b51f4e54d69
Puram Deepak <73322656+PuramDeepak@users.noreply.github.com>
2023-05-29 11:44:02 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 
 
@@ -42,16 +42,18 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} text='text-light' />
       <Alert alert={alert} />
       <div className='container'>
-        <Routes>
-          <Route exact path="/about" element={<About/>}/>
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to Analyze Below" />}/>
-        </Routes>
+        {/* <Routes> */}
+          {/* <Route exact path="/about" element={<About/>}/> */}
+          {/* <Route exact path="/" element={ */}
+          <TextForm showAlert={showAlert} heading="Enter the text to Analyze Below" />
+          {/* }/> */}
+        {/* </Routes> */}
       </div>
-        </Router>
+        {/* </Router> */}
         {/* <About /> */}
       
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">{props.title}</Link>
+        <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item mx-1">
-              <Link  className="nav-link active" aria-current="page" to="/">Home</Link>
+              <a  className="nav-link active" aria-current="page" href="/">Home</a>
             </li>
-            <Link className="nav-link active" to="/about">About</Link>
+            {/* <Link className="nav-link active" to="/about">About</Link> */}
           </ul>
           <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
             <input className="form-check-input" type="checkbox" onClick={props.toggleMode} role="switch" id="flexSwitchCheckDefault" />
